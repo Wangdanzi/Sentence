90933ea463b883f96e4c458e9c6aed8fe6c96bb7
Cindy <nuozi221179@gmail.com>
2019-11-22 16:32:13 +0800

    动态界面tab切换逻辑完成

--- pages/Dynamic/Dynamic.js
+++ pages/Dynamic/Dynamic.js
@@ -1,3 +1,14 @@
 //获取应用实例
 const app = getApp()
-Page({})
+Page({
+  clickTab (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current,
+      })
+    }
+  }
+})
