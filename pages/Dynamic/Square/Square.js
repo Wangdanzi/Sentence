d484f2c17f82105c87f83a85b2756694ad95cf2a
Cindy <nuozi221179@gmail.com>
2019-12-02 20:48:53 +0800

    动态完成

--- pages/Dynamic/Square/Square.js
+++ pages/Dynamic/Square/Square.js
@@ -1,3 +1,23 @@
 //获取应用实例
 const app = getApp()
-Page({})
+Page({
+  data:{
+    list:[
+    {
+      title:'logi',
+      id:1
+    },
+    {
+      title: 'hahahaha',
+      id: 2
+    },
+    {
+      title: 'heiheihei',
+      id: 3
+    }
+  ]
+  },
+  xiajiantou () {
+    console.log('xiajiantou');
+  }
+})
--- pages/MySelf/MySelf.js
+++ pages/MySelf/MySelf.js
@@ -1,3 +1,66 @@
 //获取应用实例
 const app = getApp()
-Page({})
+Page({
+  data:{
+    jiantou:'xia',
+    haha:1,
+    list:[
+      {
+        title:'我的消息',
+        iocn:'xiaoxi'
+      },
+      {
+        title: '我的收藏夹·',
+        iocn: 'shoucang'
+      },
+      {
+        title: '我的评论',
+        iocn: 'pinglun'
+      },
+      {
+        title: '反馈帮助',
+        iocn: 'bangzhufankui'
+      },
+      {
+        title: '推荐句读',
+        iocn: 'tuijian'
+      },
+      {
+        title: '设置',
+        iocn: 'shezhi'
+      }
+    ]
+  },
+  click(e,index){
+    console.log(e, e.currentTarget.dataset.index);
+    if (e.currentTarget.dataset.index){
+      console.log(12354);
+    if (this.data.jiantou === 'xia') {
+      this.setData({
+        jiantou: 'shangjiantou'
+      })
+
+    } else {
+      this.setData({
+        jiantou: 'xia'
+      })
+    }
+
+    }
+    // if (this.data.jiantou === 'xia') {
+    //   this.setData({
+    //     jiantou: 'shangjiantou'
+    //   })
+
+    // } else {
+    //   this.setData({
+    //     jiantou: 'xia'
+    //   })
+
+    // }
+    // e.map(item=>{
+    //   console.log(item);
+      
+    // })
+  }
+})
